ed983ef18eeae286b405dbd36fda83f72992a56f
Bruno Martins <sgtbrunoalmeida@gmail.com>
2025-09-01 20:23:53 -0300

    feat: integrate chat-based semantic search
    Added a chat interface for semantic search using LangChain's OpenAI model. Users can input questions and receive answers interactively. Enhanced usability by implementing a clear workflow with an exit option and formatted responses.

--- technical-challenges/ingestion_and_semantic_search_with_langchain_and_postgres/src/chat.py
+++ technical-challenges/ingestion_and_semantic_search_with_langchain_and_postgres/src/chat.py
@@ -1,14 +1,21 @@
+from langchain_openai import ChatOpenAI
 from search import search_prompt
 
+model = ChatOpenAI(model="gpt-4.1-mini", temperature=0.1)
+chain = search_prompt | model
 
 def main():
-    chain = search_prompt()
-
-    if not chain:
-        print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
-        return
-
-    pass
+    print("Bem-vindo ao chat de busca!")
+    print("Digite 'exit' para sair do chat.\n")
+    while True:
+        print("="*50)
+        question = input("Pergunta: ")
+        if question.lower() == "exit":
+            break
+        result = chain.invoke({"question": f"{question}"})
+        print(f"Resposta: {result.content}")
+        print("="*50, "\n")
+    print("Obrigado por usar o chat de busca!")
 
 
 if __name__ == "__main__":
