f11bda9cec7b31b18e35a12a5e58ae99bd9183c0
Bruno Martins <sgtbrunoalmeida@gmail.com>
2025-09-01 20:23:44 -0300

    feat: enhance PDF ingestion with embeddings and PGVector
    Added OpenAI embeddings and PGVector for semantic search capabilities. Improved document splitting and handling, and ensured enriched metadata filtering for better processing.

--- technical-challenges/ingestion_and_semantic_search_with_langchain_and_postgres/src/ingest.py
+++ technical-challenges/ingestion_and_semantic_search_with_langchain_and_postgres/src/ingest.py
@@ -2,20 +2,51 @@ import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
+OPENAI_EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL")
+PG_VECTOR_COLLECTION_NAME = os.getenv("PG_VECTOR_COLLECTION_NAME")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def ingest_pdf():
     loader = PyPDFLoader(PDF_PATH)
     docs = loader.load()
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=150,
+        add_start_index=False,
+    )
 
-    chunks = splitter.split_documents(docs)
+    splits = splitter.split_documents(docs)
 
-    print(f"Number of chunks: {len(chunks)}")
+    if not splits:
+        raise ValueError("No documents found")
+
+    enriched = [
+        Document(
+        page_content=d.page_content,
+        metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
+    ]
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = OpenAIEmbeddings(model=OPENAI_EMBEDDING_MODEL)
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=PG_VECTOR_COLLECTION_NAME,
+        connection=DATABASE_URL,
+        use_jsonb=True,
+    )
+    store.add_documents(documents=enriched, ids=ids)
 
 
 if __name__ == "__main__":
